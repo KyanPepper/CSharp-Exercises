4874d52ab0d7cfbf66c2c68f1155626f33b40003
KyanPepper <kyankks1@gmail.com>
2024-04-25 19:34:45 -0700

    Node and insert

---
+++ ConsoleBST/ConsoleBST/BST.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBST
+{
+    internal class BST
+    {   
+        public Node? root;
+        
+        public BST()
+        {
+            root = null;
+        }
+        public void Insert(int Data)
+        {
+            
+            if (root == null)
+            {
+                root = new Node(Data);
+            }
+
+            Node INode = new Node(Data);
+            Insert(INode, root);
+
+        }
+        private void Insert(Node INode, Node current) { 
+          
+            if(INode.data <= current.data)
+            {
+                if(current.left == null)
+                {
+                    current.left = INode;
+                }else
+                {
+                    Insert(INode, current.left);
+                }
+            }
+            else
+            {
+                if(current.right == null)
+                {
+                    current.right = INode;
+                }
+                else
+                {
+                    Insert(INode, current.right);
+                }
+
+            }
+        
+        }
+
+
+
+    }
+}
