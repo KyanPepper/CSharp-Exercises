85ea6777570ed59606be85ddbda9b729461ded94
KyanPepper <kyankks1@gmail.com>
2025-01-17 13:01:19 -0800

    PAss by value vs references

---
+++ Pass-By-Reference/Pass-By-Reference/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Hello, World!");
+
+int i = 0;
+
+changeI(i);
+Console.WriteLine( i.ToString() );
+
+changeIRef(ref i);
+
+    Console.WriteLine( i.ToString() ); 
+
+void changeI(int i)
+{
+
+    Console.WriteLine("Changing I value");
+    i = 5;
+
+}
+
+void changeIRef(ref int i)
+{
+    i = 1;
+
+    Console.WriteLine(
+         "change i  value ref "
+        );
+}
+
+
+
+
